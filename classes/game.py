2f817bce40643c0aaf99fa2aa5f0fe759dc248ab
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-01-29 18:57:04 +0000

    Using Magic

--- classes/game.py
+++ classes/game.py
@@ -14,13 +14,14 @@ class Person:
       def __init__(self, hp, mp, atk, df, magic):
           self.maxhp = hp
           self.hp = hp
-          self.maxhp = mp
+          self.maxmp = mp
           self.mp = mp
           self.atkl = atk - 10
           self.atkh = atk + 10
           self.df = df
           self.magic = magic
           self.actions = ["Attack", "Magic"]
+
 
       def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
@@ -30,8 +31,8 @@ class Person:
           mgh = self.magic[i]["dmg"] + 5
           return random.randrange(mgl, mgh)
 
-      def take_damage(self,dmg):
-          self.hp -=dmg
+      def take_damage(self, dmg):
+          self.hp -= dmg
           if self.hp < 0:
               self.hp = 0
           return self.hp
@@ -39,22 +40,22 @@ class Person:
       def get_hp(self):
           return self.hp
 
-      def get_max(self):
+      def get_max_hp(self):
           return self.maxhp
 
       def get_mp(self):
           return self.mp
 
       def get_max_mp(self):
-          return self.max_mp()
+          return self.maxmp
 
       def reduce_mp(self, cost):
           self.mp -= cost
 
-      def get_spell_name(self,i):
+      def get_spell_name(self, i):
           return self.magic[i]["name"]
 
-      def get_spell_mp_cost(self,i):
+      def get_spell_mp_cost(self, i):
           return self.magic[i]["cost"]
 
       def choose_action(self):
